c1a63149de631087e163e01e89fc197cc31d108d
leoadvance <leo_advance@163.com>
2016-11-18 19:30:02 +0800

    Qt5.4 lib

--- package/softwinner/qtlib/bin/startqt.sh
+++ package/softwinner/qtlib/bin/startqt.sh
@@ -4,8 +4,6 @@ mkdir -p /tmp/class/esp_boot
 
 export PATH=/usr/bin:/usr/sbin:/bin:/sbin
 export LD_LIBRARY_PATH=/lib/qtlib
-export QT_QWS_FONTDIR=/lib/qtlib/fonts
-export QWS_DISPLAY=Transformed:Rot270
 
 export TSLIB_TSDEVICE=/dev/input/event5
 export TSLIB_CONFFILE=/etc/ts.conf
@@ -13,12 +11,16 @@ export TSLIB_PLUGINDIR=/lib/qtlib/ts
 export TSLIB_CALIBFILE=/etc/pointercal
 export TSLIB_CONSOLEDEVICE=none
 export TSLIB_FBDEVICE=/dev/fb0
-export QWS_MOUSE_PROTO=tslib:/dev/input/event5
+
+export QT_QPA_PLATFORM=eglfs
+export QT_QPA_PLATFORM_PLUGIN_PATH=/lib/qtlib
+export QT_QPA_FONTDIR=/lib/qtlib/fonts
+export QML2_IMPORT_PATH=/lib/qtlib/qml
+export QT_QPA_GENERIC_PLUGINS=tslib:/dev/input/event5
 
 cd /bin/qtapp
 ./baklight 128
 
 logread -f >> kernel.log &
 
-export QWS_DISPLAY=Transformed:Rot270
-./BranQt4 -qws -font &
+insmod /lib/qtlib/mali.ko
